74212bf3912ef0a51327197fe98ab85b41c27e3d
Bruno Koga <brunokoga187@gmail.com>
2021-05-24 19:24:55 -0300

    remove unused string

--- test/features/data/datasource/nasa_space_media_datasource_implementation_test.dart
+++ test/features/data/datasource/nasa_space_media_datasource_implementation_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:nasa_clean_architecture/core/errors/exceptions.dart';
 import 'package:nasa_clean_architecture/core/http_client/http_client.dart';
-import 'package:nasa_clean_architecture/core/utils/keys/nasa_api_key.dart';
 import 'package:nasa_clean_architecture/features/data/datasource/space_media_data_source.dart';
 import 'package:nasa_clean_architecture/features/data/datasource/nasa_space_media_datasource_implementation.dart';
 import 'package:nasa_clean_architecture/features/data/models/space_media_model.dart';
@@ -40,9 +39,6 @@ void main() {
 
   test('should call the get method with correct url', () async {
     _successHttpClientMock();
-
-    final urlExpected =
-        'https://api.nasa.gov/planetary/apod?api_key=${NasaApiKeys.apiKey}?date=${tDateTime.year}-0${tDateTime.month}-0${tDateTime.day}';
 
     await dataSource.getSpaceMediaFromDate(
       tDateTime,
